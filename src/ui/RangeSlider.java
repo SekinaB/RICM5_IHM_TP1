5e5c496ce327ae95f4e58400609769ce4bcf2ea3
AminaDB <amina.boucherima@hotmail.com>
2018-10-04 23:57:54 +0200

    Update RangeSlider.java

--- src/ui/RangeSlider.java
+++ src/ui/RangeSlider.java
@@ -6,7 +6,7 @@ public class RangeSlider extends JSlider {
 
 	public RangeSlider() {
 		setOrientation(HORIZONTAL);
-    }
+	}
 	
 	public RangeSlider(int max, int min) throws Exception {
 		super(min,max);
@@ -14,40 +14,39 @@ public class RangeSlider extends JSlider {
 	}
 
 	@Override
-    public void updateUI() {
-        setUI(new RangeSliderUI(this));
-        updateLabelUIs();
-    }
+	public void updateUI() {
+		setUI(new RangeSliderUI(this));
+		updateLabelUIs();
+	}
 	
 	@Override
-    public int getValue() {
-        return super.getValue();
-    }
+	public int getValue() {
+		return super.getValue();
+	}
 
+	@Override
+	public void setValue(int value) {
+		int oldValue = getValue();
+		if (oldValue == value) {
+			return;
+		}
+		int oldExtent = getExtent();
+		int newValue = Math.min(Math.max(getMinimum(), value), oldValue + oldExtent);
+		int newExtent = oldExtent + oldValue - newValue;
 
-    @Override
-    public void setValue(int value) {
-        int oldValue = getValue();
-        if (oldValue == value) {
-            return;
-        }
-        int oldExtent = getExtent();
-        int newValue = Math.min(Math.max(getMinimum(), value), oldValue + oldExtent);
-        int newExtent = oldExtent + oldValue - newValue;
-
-        getModel().setRangeProperties(newValue, newExtent, getMinimum(), 
-            getMaximum(), getValueIsAdjusting());
-    }
+		getModel().setRangeProperties(newValue, newExtent, getMinimum(), 
+				getMaximum(), getValueIsAdjusting());
+	}
 	
 	public int getUpperValue() {
-        return getValue() + getExtent();
-    }
+		return getValue() + getExtent();
+	}
 	
 	public void setUpperValue(int value) {
-        int lowerValue = getValue();
-        int newExtent = Math.min(Math.max(0, value - lowerValue), getMaximum() - lowerValue);
+		int lowerValue = getValue();
+		int newExtent = Math.min(Math.max(0, value - lowerValue), getMaximum() - lowerValue);
    
-        setExtent(newExtent);
-    }
+		setExtent(newExtent);
+	}
 
 }
