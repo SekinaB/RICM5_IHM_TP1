191ea4078305e19cd6460a9737ba90430a0e5eb0
AminaDB <amina.boucherima@hotmail.com>
2018-10-04 21:15:21 +0200

    Update Main.java

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,20 +1,52 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class Main {
-	public static void main(String args[]) {
-		JFrame frame = new JFrame();
-		frame.setTitle("RangeSlider");
-		frame.setSize(400, 300);
-
-		try {
-			RangeSlider rslider = new RangeSlider(50, 0, 40, 10);
-			frame.getContentPane().add(rslider);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		frame.setVisible(true);
-	}
+/**
+ * Demo application panel to display a range slider.
+ */
+public class Main extends JPanel {
+    
+    public static void main(String[] args) {
+    	
+    	
+    		RangeSlider rangeSlider = new RangeSlider();
+    		
+    		rangeSlider.setSize(1000,700);
+		rangeSlider.setMinimum(0);
+        rangeSlider.setMaximum(10);
+        rangeSlider.setMajorTickSpacing(10);
+        rangeSlider.setMinorTickSpacing(1);
+        rangeSlider.setPaintTicks(true);
+        rangeSlider.setPaintLabels(true);
+        rangeSlider.setValue(2);
+        rangeSlider.setUpperValue(8);
+        
+        JPanel panel = new JPanel();
+        panel.add(rangeSlider);	
+        
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setTitle("Range Slider");
+        frame.getContentPane().setLayout(new BorderLayout());
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+        
+    }
 }
+
